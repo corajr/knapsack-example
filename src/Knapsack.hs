8bf58d3f449f136ac46add59a4f737a63b33768c
corajr <cora.johnson.roberson@gmail.com>
2016-10-29 23:22:23 -0400

    more general

--- src/Knapsack.hs
+++ src/Knapsack.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Knapsack ( Good(..)
                 , Knapsack
                 , emptyKnapsack
@@ -10,11 +11,12 @@ module Knapsack ( Good(..)
 
 import Control.Monad (foldM)
 import GHC.TypeLits
+import Data.Proxy (Proxy(Proxy))
 import Data.Maybe (fromMaybe)
 
 data Good = Good
-  { goodWeight :: Int
-  , goodPrice :: Int
+  { goodWeight :: Integer
+  , goodPrice :: Integer
   , goodName :: String
   } deriving (Eq, Show, Ord)
 
@@ -22,15 +24,24 @@ newtype Knapsack (n :: Nat) = Knapsack [Good]
   deriving (Eq, Show, Ord)
 
 emptyKnapsack :: Knapsack 4
-emptyKnapsack = Knapsack []
+emptyKnapsack = emptyKnapsack'
+
+emptyKnapsack' :: forall n. (KnownNat n) => Knapsack n
+emptyKnapsack' = Knapsack []
 
 mkKnapsack :: [Good] -> Maybe (Knapsack 4)
-mkKnapsack = fmap fst . foldM addGood (emptyKnapsack, 4)
+mkKnapsack = mkKnapsack'
+
+mkKnapsack' :: forall n. (KnownNat n) => [Good] -> Maybe (Knapsack n)
+mkKnapsack' = fmap fst . foldM addGood (emptyKnapsack', natVal (Proxy :: Proxy n))
   where addGood (Knapsack goods, n) x@(Good {goodWeight = w})
           | n >= w = Just (Knapsack (x:goods), n - w)
           | otherwise = Nothing
 
-getSackContents :: Knapsack a -> [Good]
+getSackCapacity :: forall n. (KnownNat n) => Knapsack n -> Integer
+getSackCapacity _ = natVal (Proxy :: Proxy n)
+
+getSackContents :: Knapsack n -> [Good]
 getSackContents (Knapsack xs) = xs
 
 stealOptimallyFrom :: [Good] -> Maybe (Knapsack 4)
