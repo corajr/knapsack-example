465c02e0be08d15e6e7ffbf3c2b9248f228caa93
corajr <cora.johnson.roberson@gmail.com>
2016-10-29 22:49:46 -0400

    make sure that knapsack can store up to 4

--- test/KnapsackSpec.hs
+++ test/KnapsackSpec.hs
@@ -1,15 +1,27 @@
 module KnapsackSpec (main, spec) where
 
 import Test.Hspec
+import Test.QuickCheck
 import Data.Maybe (isJust, isNothing)
 
 import Knapsack
+
+instance Arbitrary Good where
+  arbitrary = do
+    (NonNegative w) <- arbitrary
+    (NonNegative p) <- arbitrary
+    n <- arbitrary
+    return $ Good w p n
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
+  describe "a Knapsack 4" $ do
+    it "can contain at most 4 lbs." $ property $
+      \xs -> let weight = sum (map goodWeight xs)
+             in isJust (mkKnapsack xs) === (weight <= 4)
   describe "a Thief in the night" $ do
     let guitar = Good 1 15 "guitar"
         stereo = Good 4 30 "stereo"
